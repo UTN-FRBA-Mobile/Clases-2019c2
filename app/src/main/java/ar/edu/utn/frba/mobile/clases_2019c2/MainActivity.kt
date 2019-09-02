9b0b104af21ea88661cd4d285a5b5f075812ee6d
Emanuel <emanuel@aijunasys.com.ar>
2019-09-01 23:28:13 -0300

    Clase 03: Práctica de layouts.

--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2019c2/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2019c2/MainActivity.kt
@@ -3,25 +3,29 @@ package ar.edu.utn.frba.mobile.clases_2019c2
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener, StatusUpdateFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
+            // Solo la primera vez que corre el activity
+            // Las demás el propio manager restaura todo como estaba
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, MainFragment.newInstance(getString(R.string.hello)))
+                .replace(R.id.fragmentContainer, MainFragment.newInstance(getString(R.string.whatAreYouThinking)))
                 .commit()
         }
     }
 
-    override fun onOkTapped() {
-            AlertDialog.Builder(this)
-                .setTitle(R.string.hello)
-                .setPositiveButton(R.string.ok, { _, _ -> })
-                .create().show()
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .setCustomAnimations(R.anim.fragment_push_enter, R.anim.fragment_push_exit, R.anim.fragment_pop_enter, R.anim.fragment_pop_exit)
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2019c2/StatusUpdateFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2019c2/StatusUpdateFragment.kt
@@ -1,56 +1,41 @@
 package ar.edu.utn.frba.mobile.clases_2019c2
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import kotlinx.android.synthetic.main.fragment_main.*
-
-
-private const val ARG_TITLE = "title"
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [MainFragment.OnFragmentInteractionListener] interface
+ * [StatusUpdateFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [StatusUpdateFragment.newInstance] factory method to
  * create an instance of this fragment.
  *
  */
-class MainFragment : Fragment() {
-    private var title: String? = null
+class StatusUpdateFragment : Fragment() {
+
     private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            title = it.getString(ARG_TITLE)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_status_update, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        okButton.text = title
-        okButton.setOnClickListener {
-            onButtonPressed()
+        view.findViewById<RecyclerView>(R.id.colorsRecycler).apply {
+            layoutManager = LinearLayoutManager(context).apply {
+                orientation = LinearLayoutManager.HORIZONTAL
+            }
+            adapter = ColorsAdapter()
         }
-    }
-
-    fun onButtonPressed() {
-        listener?.showFragment(StatusUpdateFragment())
     }
 
     override fun onAttach(context: Context) {
@@ -79,7 +64,6 @@ class MainFragment : Fragment() {
      * for more information.
      */
     interface OnFragmentInteractionListener {
-        fun showFragment(fragment: Fragment)
     }
 
     companion object {
@@ -87,15 +71,10 @@ class MainFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param title Title.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment StatusUpdate.
          */
         @JvmStatic
-        fun newInstance(title: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_TITLE, title)
-                }
-            }
+        fun newInstance() =
+            StatusUpdateFragment()
     }
 }
